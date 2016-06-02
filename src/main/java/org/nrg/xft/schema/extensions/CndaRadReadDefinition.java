0a614716d7c0f17f501f6518540ab72eb1dc44ad
Rick Herrick <jrherrick@wustl.edu>
2016-06-01 20:18:10 -0500

    Added biosample collection data type and forms.

--- src/main/java/org/nrg/xft/schema/extensions/CndaRadReadDefinition.java
+++ src/main/java/org/nrg/xft/schema/extensions/CndaRadReadDefinition.java
@@ -1,25 +1,32 @@
 package org.nrg.xft.schema.extensions;
 
 import org.nrg.xft.schema.DataModelDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CndaRadReadDefinition implements DataModelDefinition{
 	@Override
 	public String getSchemaPath() {
+        _log.error("Getting the schema path");
 		return "schemas/radRead/radRead.xsd";
 	}
 
 	@Override
 	public String[] getDisplayDocs() {
-		return new String[]{"schemas/radRead/display/rad_radiologyReadData_display.xml"};
+        _log.error("Getting the display docs");
+        return new String[]{"schemas/radRead/display/rad_radiologyReadData_display.xml"};
 	}
 
 	@Override
 	public String[] getSecuredElements() {
-		return new String[]{"rad:radiologyReadData"};
+        _log.error("Getting the secured elements");
+        return new String[]{"rad:radiologyReadData"};
 	}
 
 	@Override
 	public boolean required() {
-		return false;
+		_log.error("Getting the required property");return false;
 	}
+
+	private static final Logger _log = LoggerFactory.getLogger(CndaRadReadDefinition.class);
 }
