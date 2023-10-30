4bdd85ef641f7e8063e1277d8c4e2f03e3309d50
dharlabwustl <atulkumar@wustl.edu>
2023-10-30 15:10:50 -0500

    First comment

--- src/main/java/org/nrg/xnat/atul/subjectmapping/entities/SubjectMapping.java
+++ src/main/java/org/nrg/xnat/atul/subjectmapping/entities/SubjectMapping.java
@@ -7,7 +7,7 @@
  * Released under the Simplified BSD.
  */
 
-package org.nrg.xnat.workshop.subjectmapping.entities;
+package org.nrg.xnat.atul.subjectmapping.entities;
 
 import org.nrg.framework.orm.hibernate.AbstractHibernateEntity;
 
