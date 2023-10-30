ac4bb687766cfca33ac50908aed5c2ec2e9cafaf
dharlabwustl <atulkumar@wustl.edu>
2023-10-30 16:32:52 -0500

    First comment

--- src/main/java/org/apache/turbine/app/xnat/modules/screens/XDATScreen_edit_atul_nwuCollection.java
+++ src/main/java/org/apache/turbine/app/xnat/modules/screens/XDATScreen_edit_atul_nwuCollection.java
@@ -26,59 +26,59 @@ import java.util.List;
 public class XDATScreen_edit_atul_nwuCollection extends EditSubjectAssessorScreen {
     @Override
     public String getElementName() {
-        return AtulNwucollection.SCHEMA_ELEMENT_NAME;
+        return String.valueOf(1); //AtulNwucollection.SCHEMA_ELEMENT_NAME;
     }
-
-    @Override
-    public void finalProcessing(RunData data, Context context) {
-        super.finalProcessing(data,context);
-
-        try {
-            final String subjectId;
-            if (!context.containsKey("subjectId")) {
-                if (context.containsKey("part")) {
-                    final Object part = context.get("part");
-                    if (part instanceof XnatSubjectdata) {
-                        subjectId = ((XnatSubjectdata) part).getId();
-                    } else if (part instanceof XFTItem) {
-                        subjectId = ((XFTItem) part).getStringProperty("ID");
-                    } else {
-                        subjectId = part.toString();
-                    }
-                    context.put("subjectId", subjectId);
-                } else {
-                    subjectId = (String) context.get("part_id");
-                }
-            } else {
-                subjectId = (String) context.get("subjectId");
-            }
-
-            if (!context.containsKey("label")) {
-                final XnatSubjectdata               subject    = XnatSubjectdata.getXnatSubjectdatasById(subjectId, XDAT.getUserDetails(), false);
-                final List<XnatSubjectassessordata> nwus = subject.getExperiments_experiment(AtulNwucollection.SCHEMA_ELEMENT_NAME);
-                final String subjectLabel = subject.getLabel();
-                int index = 1;
-                String label = null;
-                while (label == null) {
-                    final String test = subjectLabel + "_BIO" + String.format("%02d", index);
-                    boolean matches = false;
-                    for (final XnatSubjectassessordata nwu : nwus) {
-                        if (nwu.getLabel().equals(test)) {
-                            matches = true;
-                            index++;
-                            break;
-                        }
-                    }
-                    if (!matches) {
-                        label = test;
-                    }
-                }
-                context.put("label", label);
-            }
-        } catch (XFTInitException | ElementNotFoundException | FieldNotFoundException e) {
-            final String message = "An error occurred trying to get the subject ID when creating a nwu collection assessor.";
-            log.error(message, e);
-            throw new RuntimeException(message, e);
-        }
-    }
+//
+//    @Override
+//    public void finalProcessing(RunData data, Context context) {
+//        super.finalProcessing(data,context);
+//
+//        try {
+//            final String subjectId;
+//            if (!context.containsKey("subjectId")) {
+//                if (context.containsKey("part")) {
+//                    final Object part = context.get("part");
+//                    if (part instanceof XnatSubjectdata) {
+//                        subjectId = ((XnatSubjectdata) part).getId();
+//                    } else if (part instanceof XFTItem) {
+//                        subjectId = ((XFTItem) part).getStringProperty("ID");
+//                    } else {
+//                        subjectId = part.toString();
+//                    }
+//                    context.put("subjectId", subjectId);
+//                } else {
+//                    subjectId = (String) context.get("part_id");
+//                }
+//            } else {
+//                subjectId = (String) context.get("subjectId");
+//            }
+//
+//            if (!context.containsKey("label")) {
+//                final XnatSubjectdata               subject    = XnatSubjectdata.getXnatSubjectdatasById(subjectId, XDAT.getUserDetails(), false);
+//                final List<XnatSubjectassessordata> nwus = subject.getExperiments_experiment(AtulNwucollection.SCHEMA_ELEMENT_NAME);
+//                final String subjectLabel = subject.getLabel();
+//                int index = 1;
+//                String label = null;
+//                while (label == null) {
+//                    final String test = subjectLabel + "_BIO" + String.format("%02d", index);
+//                    boolean matches = false;
+//                    for (final XnatSubjectassessordata nwu : nwus) {
+//                        if (nwu.getLabel().equals(test)) {
+//                            matches = true;
+//                            index++;
+//                            break;
+//                        }
+//                    }
+//                    if (!matches) {
+//                        label = test;
+//                    }
+//                }
+//                context.put("label", label);
+//            }
+//        } catch (XFTInitException | ElementNotFoundException | FieldNotFoundException e) {
+//            final String message = "An error occurred trying to get the subject ID when creating a nwu collection assessor.";
+//            log.error(message, e);
+//            throw new RuntimeException(message, e);
+//        }
+//    }
 }
