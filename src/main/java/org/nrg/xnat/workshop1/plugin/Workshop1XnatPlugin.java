7d03fdac3ab7f29ae0fb6975a0e2fdaf80ac1533
dharlabwustl <atulkumar@wustl.edu>
2023-10-30 00:25:55 -0500

    First comment

--- src/main/java/org/nrg/xnat/workshop1/plugin/Workshop1XnatPlugin.java
+++ src/main/java/org/nrg/xnat/workshop1/plugin/Workshop1XnatPlugin.java
@@ -12,7 +12,6 @@ package org.nrg.xnat.workshop1.plugin;
 import org.nrg.framework.annotations.XnatDataModel;
 import org.nrg.framework.annotations.XnatPlugin;
 import org.nrg.xdat.bean.Workshop1Biosample1collectionBean;
-import org.nrg.xdat.bean.RadRadiologyreaddataBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
