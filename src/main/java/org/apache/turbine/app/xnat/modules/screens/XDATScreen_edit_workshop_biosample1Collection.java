c34518214925d286c9b42a3c0fa567a3147b349f
dharlabwustl <atulkumar@wustl.edu>
2023-10-29 20:38:10 -0500

    First comment

--- src/main/java/org/apache/turbine/app/xnat/modules/screens/XDATScreen_edit_workshop_biosample1Collection.java
+++ src/main/java/org/apache/turbine/app/xnat/modules/screens/XDATScreen_edit_workshop_biosample1Collection.java
@@ -1,5 +1,5 @@
 /*
- * xnat-workshop: org.apache.turbine.app.xnat.modules.screens.XDATScreen_edit_workshop_biosampleCollection
+ * xnat-workshop: org.apache.turbine.app.xnat.modules.screens.XDATScreen_edit_workshop_biosample1Collection
  * XNAT http://www.xnat.org
  * Copyright (c) 2017, Washington University School of Medicine
  * All Rights Reserved
@@ -12,7 +12,7 @@ package org.apache.turbine.app.xnat.modules.screens;
 import org.apache.turbine.util.RunData;
 import org.apache.velocity.context.Context;
 import org.nrg.xdat.XDAT;
-import org.nrg.xdat.om.WorkshopBiosamplecollection;
+import org.nrg.xdat.om.WorkshopBiosample1collection;
 import org.nrg.xdat.om.XnatSubjectassessordata;
 import org.nrg.xdat.om.XnatSubjectdata;
 import org.nrg.xft.XFTItem;
@@ -23,10 +23,10 @@ import org.nrg.xnat.turbine.modules.screens.EditSubjectAssessorScreen;
 
 import java.util.List;
 
-public class XDATScreen_edit_workshop_biosampleCollection extends EditSubjectAssessorScreen {
+public class XDATScreen_edit_workshop_biosample1Collection extends EditSubjectAssessorScreen {
     @Override
     public String getElementName() {
-        return WorkshopBiosamplecollection.SCHEMA_ELEMENT_NAME;
+        return WorkshopBiosample1collection.SCHEMA_ELEMENT_NAME;
     }
 
     @Override
@@ -55,15 +55,15 @@ public class XDATScreen_edit_workshop_biosampleCollection extends EditSubjectAss
 
             if (!context.containsKey("label")) {
                 final XnatSubjectdata               subject    = XnatSubjectdata.getXnatSubjectdatasById(subjectId, XDAT.getUserDetails(), false);
-                final List<XnatSubjectassessordata> biosamples = subject.getExperiments_experiment(WorkshopBiosamplecollection.SCHEMA_ELEMENT_NAME);
+                final List<XnatSubjectassessordata> biosample1s = subject.getExperiments_experiment(WorkshopBiosample1collection.SCHEMA_ELEMENT_NAME);
                 final String subjectLabel = subject.getLabel();
                 int index = 1;
                 String label = null;
                 while (label == null) {
                     final String test = subjectLabel + "_BIO" + String.format("%02d", index);
                     boolean matches = false;
-                    for (final XnatSubjectassessordata biosample : biosamples) {
-                        if (biosample.getLabel().equals(test)) {
+                    for (final XnatSubjectassessordata biosample1 : biosample1s) {
+                        if (biosample1.getLabel().equals(test)) {
                             matches = true;
                             index++;
                             break;
@@ -76,7 +76,7 @@ public class XDATScreen_edit_workshop_biosampleCollection extends EditSubjectAss
                 context.put("label", label);
             }
         } catch (XFTInitException | ElementNotFoundException | FieldNotFoundException e) {
-            final String message = "An error occurred trying to get the subject ID when creating a biosample collection assessor.";
+            final String message = "An error occurred trying to get the subject ID when creating a biosample1 collection assessor.";
             log.error(message, e);
             throw new RuntimeException(message, e);
         }
--- src/main/java/org/nrg/xnat/workshop/plugin/WorkshopXnatPlugin.java
+++ src/main/java/org/nrg/xnat/workshop/plugin/WorkshopXnatPlugin.java
@@ -11,15 +11,15 @@ package org.nrg.xnat.workshop.plugin;
 
 import org.nrg.framework.annotations.XnatDataModel;
 import org.nrg.framework.annotations.XnatPlugin;
-import org.nrg.xdat.bean.WorkshopBiosamplecollectionBean;
+import org.nrg.xdat.bean.WorkshopBiosample1collectionBean;
 import org.nrg.xdat.bean.RadRadiologyreaddataBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @XnatPlugin(value = "workshopPlugin", name = "XNAT 1.7 Workshop 2016 Plugin", entityPackages = "org.nrg.xnat.workshop.entities",
-            dataModels = {@XnatDataModel(value = WorkshopBiosamplecollectionBean.SCHEMA_ELEMENT_NAME,
-                                         singular = "Biosample Collection",
-                                         plural = "Biosample Collections"),
+            dataModels = {@XnatDataModel(value = WorkshopBiosample1collectionBean.SCHEMA_ELEMENT_NAME,
+                                         singular = "Biosample1 Collection",
+                                         plural = "Biosample1 Collections"),
                           @XnatDataModel(value = RadRadiologyreaddataBean.SCHEMA_ELEMENT_NAME,
                                          singular = "Radiology Read",
                                          plural = "Radiology Reads")})
